437c6b16fb5485fe11390b50ae53577a4dda8026
Miguel Peixe <miguel@cardume.art.br>
2015-11-21 20:35:07 -0200

    add acf as submodule

--- functions.php
+++ functions.php
@@ -23,7 +23,7 @@ if(!class_exists('Acf')) {
   // 3. Hide ACF field group menu item
   add_filter('acf/settings/show_admin', '__return_false');
 
-  // require_once(STYLESHEETPATH . '/inc/acf/acf.php');
+  require_once(STYLESHEETPATH . '/inc/acf/acf.php');
 }
 
 /*
