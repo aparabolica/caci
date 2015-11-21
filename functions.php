8a71c52d8e0ed9c52a2803711eb62f4869352f2c
Miguel Peixe <miguel@cardume.art.br>
2015-11-21 20:33:04 -0200

    remove acf

--- functions.php
+++ functions.php
@@ -1,6 +1,32 @@
 <?php
 
 /*
+ * ACF
+ */
+
+if(!class_exists('Acf')) {
+
+  function vindig_acf_settings_path( $path ) {
+    $path = get_stylesheet_directory() . '/inc/acf/';
+    return $path;
+  }
+  add_filter('acf/settings/path', 'vindig_acf_settings_path');
+
+  function vindig_acf_settings_dir( $dir ) {
+    // update path
+    $dir = get_stylesheet_directory_uri() . '/inc/acf/';
+    // return
+    return $dir;
+  }
+  add_filter('acf/settings/dir', 'vindig_acf_settings_dir');
+
+  // 3. Hide ACF field group menu item
+  add_filter('acf/settings/show_admin', '__return_false');
+
+  // require_once(STYLESHEETPATH . '/inc/acf/acf.php');
+}
+
+/*
  * Theme WP features
  */
 add_theme_support('post-thumbnails');
