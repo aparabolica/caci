f18231c7d6c3f9027024f18f4bf8d16334a82d61
Miguel Peixe <miguel@cardume.art.br>
2016-05-30 00:05:36 -0300

    contact fixes

--- inc/contact.php
+++ inc/contact.php
@@ -25,16 +25,20 @@ class Vindig_Contact {
   }
 
   function contact($data) {
+
     $value = array(
       'name' => $data['name'],
       'email' => $data['email'],
-      'message' => $data['body']
+      'body' => $data['body']
     );
 
     // Send email
     $email = get_option('admin_email');
     $body = '<p>Nova mensagem de <strong>' . $value['name'] . '</strong></p><p>Email: <strong>' . $value['email'] . '</strong></p><p><strong>Mensagem</strong>:</p><p><blockquote>' . $data['body'] . '</blockquote></p>';
     $headers = array('Content-Type:text/html;charset=UTF-8');
+
+    error_log(print_r($value, true));
+    error_log(print_r($email, true));
 
     $mailed = wp_mail($email, '[CACI] Nova mensagem de ' . $value['name'], $body, $headers);
 
